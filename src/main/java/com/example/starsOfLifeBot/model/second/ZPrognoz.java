a2e574e755545bc9c2d44c63e839cd04fc25da27
Илья <iliya168@yandex.ru>
2026-02-28 12:50:13 +0300

    fix spring constuctor annotation

--- src/main/java/com/example/starsOfLifeBot/model/second/ZPrognoz.java
+++ src/main/java/com/example/starsOfLifeBot/model/second/ZPrognoz.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 @Table(name = "zprognoz")
